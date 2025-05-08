4aa377837b0da8b21eae4fcfa6aa14c45c0ff81b
Tanusha Choudhary <144540796+tanushachoudhary@users.noreply.github.com>
2025-05-08 20:31:26 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "$";
-  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL||"https://prescripto-backend-d6ek.onrender.com";
   const [doctors, setDoctors] = useState([]);
   const [token, setToken] = useState(
     localStorage.getItem("token") ? localStorage.getItem("token") : false
