605ad2b759067325f5fc6da35ea829baffb71e9f
Tanusha <tanushachoudhary18@gmail.com>
2025-05-12 23:35:40 +0530

    doctor panel

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -15,6 +15,7 @@ const DoctorContextProvider = (props) => {
 
   const [appointments, setAppointments] = useState([]);
   const [dashData, setDashData] = useState(false);
+  const [profileData, setProfileData] = useState(false);
 
   const getAppointments = async () => {
     try {
@@ -81,6 +82,22 @@ const DoctorContextProvider = (props) => {
       }
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const getProfileData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "api/doctor/profile", {
+        headers: { dToken },
+      });
+
+      if (data.success) {
+        setProfileData(data.profileData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
       toast.error(error.message);
     }
   };
@@ -97,6 +114,9 @@ const DoctorContextProvider = (props) => {
     dashData,
     setDashData,
     getDashData,
+    profileData,
+    setProfileData,
+    getProfileData,
   };
 
   return (
--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -5,7 +5,9 @@ import {
   appointmentsDoctor,
   doctorDashboard,
   doctorList,
+  doctorProfile,
   loginDoctor,
+  updateDoctorProfile,
 } from "../controllers/doctorController.js";
 import authDoctor from "./../middlewares/authDoctor.js";
 
@@ -17,5 +19,7 @@ doctorRouter.get("/appointments", authDoctor, appointmentsDoctor);
 doctorRouter.post("/complete-appointment", authDoctor, appointmentComplete);
 doctorRouter.post("/cancel-appointment", authDoctor, appointmentCancel);
 doctorRouter.get("/dashboard", authDoctor, doctorDashboard);
+doctorRouter.get("/profile", authDoctor, doctorProfile);
+doctorRouter.post("/update-profile", authDoctor, updateDoctorProfile);
 
 export default doctorRouter;
